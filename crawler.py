bb4b422224957373860869310b0650f1f48010a1
sanju <sanjusk08gmail.com>
2022-08-17 11:40:39 +0530

    implemented download_lyrics functions to download lyrics implemented generate_name function

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import os
 
 from bs4 import BeautifulSoup
 import requests
@@ -7,10 +8,23 @@ import requests
 logger = None
 
 
+def generate_name(name: str) -> str:
+    '''returns a new string after replacing undesired chars with '-' '''
+    char_to_replace = ['/', ' ']
+    for char in char_to_replace:
+        name = name.replace(char, '-')
+    return name.lower()
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Web crawler")
     parser.add_argument(
         "-d", "--debug", help="Enable debug logging", action="store_true"
+    )
+    parser.add_argument(
+        "-c", "--count", type=int, help="Number of artists and songs to crawl", default=2
+    )
+    parser.add_argument(
+       "-u", "--url", type=str, help="Base url for artist list", default= "https://www.songlyrics.com/top-artists-lyrics.html"
     )
     return parser.parse_args()
 
@@ -28,7 +42,7 @@ def configure_logging(level=logging.INFO):
     logger.addHandler(screen_handler)
 
 
-def get_artists(base):
+def get_artists(base: str, count: int=2) -> dict:
     """returns a dictionary of artists with the url to their song list"""
     artists = {}
     logger.debug(f"requesting {base} ...")
@@ -37,37 +51,32 @@ def get_artists(base):
     soup = BeautifulSoup(res.content, "lxml")
     tracklist = soup.find("table", attrs={"class": "tracklist"})
     headings = tracklist.find_all("h3")
-    if headings:
-        logger.debug("artist list parsed successfully")
-    else:
+    if not headings:
         logger.debug("could'nt parse headings successfully")
-    for heading in headings:
+    for heading in headings[0:count]:
         artists[heading.text] = heading.a["href"]
     return artists
 
 
-def get_song_list(base):
+def get_song_list(url: str, count: int=2) -> dict:
     """returns a dictionary of songs with the url to the song lyrics"""
     songs = {}
-    logger.debug(f"requesting {base} ...")
-    res = requests.get(base)
+    logger.debug(f"requesting {url} ...")
+    res = requests.get(url)
     logger.debug(f"status code: {res.status_code}")
     soup = BeautifulSoup(res.content, "lxml")
     tracklist = soup.find("table", attrs={"class": "tracklist"})
     links = tracklist.find_all("a")
-    if links:
-        logger.debug("song list parsed successfully")
-    else:
+    if not links:
         logger.debug("could'nt parse headings successfully")
-    for link in links:
+    for link in links[0:count]:
         songs[link.text] = link["href"]
     return songs
 
-
-def get_lyrics(base):
+def get_lyrics(url: str) -> str:
     "returns song lyrics parsed from the given url"
-    logger.debug(f"requesting {base} ...")
-    res = requests.get(base)
+    logger.debug(f"requesting {url} ...")
+    res = requests.get(url)
     logger.debug(f"status code: {res.status_code}")
     soup = BeautifulSoup(res.content, "lxml")
     lyrics = soup.find("p", attrs={"id": "songLyricsDiv"})
@@ -78,15 +87,30 @@ def get_lyrics(base):
     return lyrics.text
 
 
+def download_lyrics(download_dir: str, count: int, base_url: str):
+    artists = get_artists("https://www.songlyrics.com/top-artists-lyrics.html", count)
+    
+    for artist, url in artists.items():
+        song_list = get_song_list(url, count)
+        for song, url in  song_list.items():
+            artist_name = generate_name(artist)
+            os.makedirs(os.path.join(download_dir, artist_name), exist_ok=True)
+            song_name = generate_name(song)
+            with open(os.path.join(download_dir, artist_name, f'{song_name}.txt'), 'w') as f:
+                byte_count = f.write(get_lyrics(url))
+                if byte_count < 5:
+                    logger.warn(' %s too small', str(os.path.join(download_dir, artist_name, f'{song_name}.txt')))
+
+
+
+
 def main():
     args = parse_args()
     if args.debug:
-        configure_logging(logging.DEBUG)
+        configure_logging()
     else:
         configure_logging(logging.INFO)
 
-    artists = get_artists("https://www.songlyrics.com/top-artists-lyrics.html")
-
-
+    download_lyrics('artists', args.count, args.url) 
 if __name__ == "__main__":
     main()
