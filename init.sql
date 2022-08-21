93d57db98119c6d7d7c777ebfafc9123683c72f5
sanju <sanjusk08gmail.com>
2022-08-21 16:45:48 +0530

    added init.sql

---
+++ init.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS songs;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL);
+
+
+CREATE TABLE songs (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    artist_id INTEGER references artists(id),
+    lyrics TEXT NOT NULL
+);
+
